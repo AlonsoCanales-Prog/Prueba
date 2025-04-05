0dcb7bea4b081ab4d09f79e90f019dc141373aaa
Alonso Canales Padilla <alonso.canales7239@alumnos.udg.mx>
2025-04-04 22:15:46 -0600

    neg

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")  # Esto define la ruta de la aplicación
 def home():
     return "Hola desde Render!"
 
-if __name__=='main':
-    app.run()
+if __name__ == "__main__":  # Corregido
+    app.run(host="0.0.0.0", port=10000)  # Render usa un puerto asignado automáticamente
